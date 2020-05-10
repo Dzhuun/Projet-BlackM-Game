f4cd3fc2440893a8692cbc780b099e5ff390d5b0
Clement Laurenti <clement.laurenti@hotmail.fr>
2020-05-10 10:34:15 +0200

    - Lobby créé - Effet de blur créé

---
+++ Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static string PLAYER_LOADED_LEVEL = "PlayerLoadedLevel";
+
+    public static string PLAYER_READY = "PlayerReady";
+
+    public static string GAME_SCENE = "room";
+
+    public static Color GetColor(int colorChoice)
+    {
+        switch (colorChoice)
+        {
+            case 0: return Color.red;
+            case 1: return Color.green;
+            case 2: return Color.blue;
+            case 3: return Color.yellow;
+            case 4: return Color.cyan;
+            case 5: return Color.grey;
+            case 6: return Color.magenta;
+            case 7: return Color.white;
+        }
+
+        return Color.black;
+    }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
